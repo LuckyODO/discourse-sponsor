e2015a93d3d7a9c9e31c8402ea095fcce7118e4b
LuckyODO <luckyovvo@gmail.com>
2026-01-11 16:03:36 +0800

    Add sponsor page amounts and payment buttons

---
+++ assets/javascripts/discourse/controllers/sponsor.js
@@ -0,0 +1,29 @@
+import Controller from "@ember/controller";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { inject as service } from "@ember/service";
+
+export default class SponsorController extends Controller {
+  @service siteSettings;
+
+  @tracked amount = "";
+
+  get presetAmounts() {
+    const raw = this.siteSettings.sponsor_preset_amounts || "";
+
+    return raw
+      .split("|")
+      .map((value) => value.trim())
+      .filter((value) => value.length > 0);
+  }
+
+  @action
+  selectPreset(amount) {
+    this.amount = amount;
+  }
+
+  @action
+  updateAmount(event) {
+    this.amount = event.target.value;
+  }
+}
